95b75e952c715cb6eb749f7409c3bc759e60463f
GabsMeloTI <rm550202@fiap.com.br>
2025-03-06 17:31:01 -0300

    updated error

--- db/query/advertisement.sql
+++ db/query/advertisement.sql
@@ -58,4 +58,3 @@ SELECT id, destination, origin, pickup_date, delivery_date, expiration_date, tit
 FROM public.advertisement
 WHERE status=true
 ORDER BY expiration_date;
-p.CreateAdvertisementRequest
