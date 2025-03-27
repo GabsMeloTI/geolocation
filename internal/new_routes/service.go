8d6949869be3b1a839274ae72b2572409b09f319
GabsMeloTI <rm550202@fiap.com.br>
2025-03-27 11:55:41 -0300

    generated swagger

--- internal/new_routes/service.go
+++ internal/new_routes/service.go
@@ -432,7 +432,9 @@ func (s *Service) savedRoutes(ctx context.Context, PublicOrPrivate, origin, dest
 			return 0, err
 		}
 	} else {
+		fmt.Println(existingRoute.NumberRequest)
 		newCount := existingRoute.NumberRequest + 1
+		fmt.Println(newCount)
 		err = s.InterfaceService.UpdateNumberOfRequestRequest(ctx, db.UpdateNumberOfRequestParams{
 			ID:            existingRoute.ID,
 			NumberRequest: newCount,
@@ -441,6 +443,7 @@ func (s *Service) savedRoutes(ctx context.Context, PublicOrPrivate, origin, dest
 			return 0, err
 		}
 		routeHistID = existingRoute.ID
+		fmt.Println(routeHistID)
 	}
 
 	_, err = s.InterfaceService.CreateSavedRoutes(ctx, db.CreateSavedRoutesParams{
